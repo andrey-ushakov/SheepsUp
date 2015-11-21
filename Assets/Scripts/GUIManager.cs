6a8c1dc642fdcc32941a83a5a8bd90d03f2f26c7
pangboww <pangboww@gmail.com>
2015-11-21 21:39:54 +0100

    update guimanager

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -18,26 +18,32 @@ public class GUIManager : MonoBehaviour {
 	void Start () {
 		dreamStatus = gm.dreamStatus;
 		score = gm.score;
+
+		dreamStatus = 1f;
 	}
 
 	void OnGUI() {
 		Camera camera = Camera.main;
 
-		Vector3 position = camera.ViewportToScreenPoint (new Vector3 (0.6f, 0.1f, 1f));
+		Vector3 position = camera.ViewportToScreenPoint (new Vector3 (0.5f, 0.1f, 1f));
 		Vector3 size = camera.ViewportToScreenPoint (new Vector3 (0.4f, 0.1f, 1f));
 
-		DrawProgress (position.x, position.y, size.x, size.y, 0.6f);
+		DrawProgress (position.x, position.y, size.x, size.y, 1f);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
+		dreamStatus -= 0.3f * Time.deltaTime;
+		if (dreamStatus <= 0.1)
+			dreamStatus = 1f;
 		
 	}
 
 	void DrawProgress(float x, float y, float l, float h, float progress)
 	{
-		GUI.DrawTexture(new Rect(x + l/7, y+ h/3, l * progress, h/3), bar_content);
+		GUI.DrawTexture(new Rect(x + l/7, y+ h/3, l - l/7, h/3), bar_content);
 		GUI.DrawTexture(new Rect(x, y, l, h), bar_broder);
+		GUI.DrawTexture (new Rect (x + l / 7f + (l * 0.84f)  * progress, y, l / 50, h), bar_pin);
 	}
 }
