517c2868316c2bd78e4012e0a30e41d4e273a6e2
AndreyU <ushakov.andrey.34@gmail.com>
2015-11-21 19:27:37 +0100

    Update scene

--- Assets/Scripts/PatternManagerLayout1.cs
+++ Assets/Scripts/PatternManagerLayout1.cs
@@ -3,16 +3,12 @@ using System.Collections;
 
 public class PatternManagerLayout1 : PatternManager {
 
-	// Use this for initialization
 	void Start () {
-		period = 45 / (GlobalVariables.layout1Speed);
+		float w = _grass.GetComponent<SpriteRenderer>().sprite.rect.width;
+		//Debug.Log (w);
+		period = (Mathf.Sqrt(w)-0.5f) / (GlobalVariables.layout1Speed);
 		curTime	= period;
 		generateTime = period;
 		generateGrass(_generateInitPos);
 	}
-	
-	// Update is called once per frame
-	/*void Update () {
-	
-	}*/
 }
