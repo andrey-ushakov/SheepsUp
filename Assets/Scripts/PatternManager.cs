89b39e659c97fa6b06b8859e941c3dd1f1a05e81
AndreyU <ushakov.andrey.34@gmail.com>
2015-11-21 10:59:45 +0100

    Added grass prefab

--- Assets/Scripts/PatternManager.cs
+++ Assets/Scripts/PatternManager.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class PatternManager : MonoBehaviour {
 
-	// Use this for initialization
+	[SerializeField]
+	private GameObject _grass;
+	
 	void Start () {
-	
+		Camera camera = Camera.main;
+		Vector3 p = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+		p.x += 1;
+		p.y += 1;
+
+		GameObject.Instantiate(_grass, p, Quaternion.identity);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
