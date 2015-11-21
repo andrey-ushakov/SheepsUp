4ec6f03ef9c4a49b6ef9e718a392b3fc4c9e1d53
AndreyU <ushakov.andrey.34@gmail.com>
2015-11-21 21:05:38 +0100

    Background scrool

--- Assets/OffsetScroller.cs
+++ Assets/OffsetScroller.cs
@@ -3,20 +3,34 @@ using System.Collections;
 
 public class OffsetScroller : MonoBehaviour {
 
-	public float scrollSpeed;
+	[SerializeField]
+	private float speed = 0.5f;
+
+
+	void Start () {
+	}
+
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset;
+	}
+
+
+
+	/*public float scrollSpeed;
 	private Vector2 savedOffset;
 	
 	void Start () {
-		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("layout1");
 	}
 	
 	void Update () {
 		float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
 		Vector2 offset = new Vector2 (savedOffset.x, y);
-		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("layout1", offset);
 	}
 	
 	void OnDisable () {
-		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
-	}
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("layout1", savedOffset);
+	}*/
 }
--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -6,11 +6,11 @@ public static class GlobalVariables {
 	[SerializeField]
 	public static float grassSpeed = 2f;
 	[SerializeField]
-	public static float layout1Speed = 3f;
+	public static float layout1Speed = 5f;
 	[SerializeField]
-	public static float layout2Speed = 5f;
+	public static float layout2Speed = 3f;
 	[SerializeField]
-	public static float layout3Speed = 7f;
+	public static float layout3Speed = 2f;
 
 	/*void Start () {
 	
