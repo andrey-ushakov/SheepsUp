6573d3ed93b773e18bfad093fc243ba4e9a8f38e
AndreyU <ushakov.andrey.34@gmail.com>
2015-11-22 01:28:09 +0100

    Added menu.

---
+++ Assets/Scripts/menu/Title.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	[SerializeField]
+	private float _x = 0.5f;
+	[SerializeField]
+	private float _y = 0.5f;
+	[SerializeField]
+	private float _z = 9;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 p = Camera.main.ViewportToWorldPoint (new Vector3(_x, _y, _z));
+		Debug.Log (p);
+		this.transform.position = p;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
