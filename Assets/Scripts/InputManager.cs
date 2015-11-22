f0de35065d700627bea32fb34a1e45e64266b80f
AndreyU <ushakov.andrey.34@gmail.com>
2015-11-22 01:55:05 +0100

    Changed game Button textures

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,7 +6,12 @@ public delegate void ButtonAClickedEventHandler(object sender);
 public delegate void ButtonBClickedEventHandler(object sender);
 
 public class InputManager : MonoBehaviour {
-
+	
+	public Texture buttonA;
+	public Texture buttonAPressed;
+	public Texture buttonB;
+	public Texture buttonBPressed;
+	public GUIStyle mystyle;
 	public static event ButtonAClickedEventHandler buttonAClicked;
 	public static event ButtonBClickedEventHandler buttonBClicked;
 
@@ -16,11 +21,11 @@ public class InputManager : MonoBehaviour {
 		Vector3 p2 = camera.ViewportToScreenPoint (new Vector3 (0.8f, 0.8f, 1f));
 
 		//Button buttonA = 
-		if (GUI.Button (new Rect (p1.x, p1.y, 100, 100), "A button")) {
+		if (GUI.Button (new Rect (p1.x, p1.y, 100, 100), buttonA, mystyle)) {
 			OnButtonAClicked();
 		}
 		//Button buttonB = 
-		if (GUI.Button (new Rect (p2.x, p2.y, 100, 100), "B button")){
+		if (GUI.Button (new Rect (p2.x, p2.y, 100, 100), buttonB, mystyle)){
 			OnButtonBClicked();
 		}
 
